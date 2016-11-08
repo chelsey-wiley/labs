f48df5c9b5005443050d1ebb809ecdbb3961ba7a
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-11-08 13:41:15 -0600

    completed lab  4

---
+++ functions/lab-04.js
@@ -0,0 +1,13 @@
+
+//function goes here
+function avg(a, b, c) {
+  var answer = (a + b + c)/avg.length;
+  return answer;
+}
+
+console.log(avg(10, 6, 2));
+
+
+/*COMPLETED
+Write a function that takes three parameters and computes and returns the average.
+*/
