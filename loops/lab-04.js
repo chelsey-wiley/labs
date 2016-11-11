0e958509f28fa17a722b48b8cee699f37101dfd7
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-11-11 09:16:33 -0600

    syntax corrections

--- loops/lab-04.js
+++ loops/lab-04.js
@@ -1,11 +1,14 @@
 var nums = [1, 6, 42, 3, 9];
 var sum = 0;
 
-//loop goes here
+for (var i = 0; i < nums.length; i++){
+  var value = nums[i];
+  sum = sum + value;
+}
 
-console.log(sum);
-
-/*
+  console.log(sum);
+  
+/* COMPLETED
 Write a while loop and compute the sum of the values, then console log it.
 Expected output:
 61
